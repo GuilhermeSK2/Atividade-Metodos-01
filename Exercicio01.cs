b29f545dbfc087b5a1d829245bf335622557b484
GuilhermeSK2 <iosfantasma08@gmail.com>
2024-06-26 07:01:18 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio01.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtividadeMetodos01
+{
+    internal class Exercicio01
+    {
+        public static void ImprimirSaudacao() 
+        {
+            Console.WriteLine("Olá, bem vindo ao curso de C#!");
+        }
+
+        public static void Main(string [] args)
+            {
+                ImprimirSaudacao();
+            }
+    }
+}
---
+++ Exercicio09.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtividadeMetodos01
+{
+    internal class Exercicio09
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
